ad5df77091848932dcf692d0e2e3cb0937c68a88
AyWizard <aywizard@gmail.com>
2016-02-24 12:56:07 -0800

    fix bug

--- hw/arm/bcm2835_todo.c
+++ hw/arm/bcm2835_todo.c
@@ -23,22 +23,29 @@ typedef struct {
 static uint64_t bcm2835_todo_read(void *opaque, hwaddr offset,
     unsigned size)
 {
-    printf("[QEMU] TOS GPIO: read(%x)\n", (int)offset);
-    /* "Unlocks" RiscOS boot */
-    if (offset == 0x980010) {
-        return 0xffffffff;
-    }
+    fprintf(stderr, "[QEMU] TOS GPIO: read");
+    bcm2835_todo_state *s = (bcm2835_todo_state *)opaque;
+    
+    fprintf(stderr, "[QEMU] TOS GPIO: read %x from 202000%x size:(%d)\n", (int)*(s->gpio_shm+(int)offset/4), (int)offset, (int)size);
 
-    return 0;
+    return (unsigned)*(s->gpio_shm+(int)offset/4);
 }
 
 static void bcm2835_todo_write(void *opaque, hwaddr offset,
     uint64_t value, unsigned size)
 {
-    printf("[QEMU] TOS GPIO: write to (%x) %lx\n", (int)offset, value);
+    fprintf(stderr,"[QEMU] TOS GPIO: write %x to 202000%x \n", (int)value, (int)offset);
     bcm2835_todo_state *s = (bcm2835_todo_state *)opaque;
-    *(s->gpio_shm+4) = 0xff;
-
+    *(s->gpio_shm+(int)offset/4) = (unsigned)value;
+    if (((int)offset == 0x1c) || ((int)offset == 0x20)) {
+        // write to 0x34 or 0x38
+        *(s->gpio_shm+(int)offset/4+6) |= (unsigned)value;
+        fprintf(stderr,"[QEMU] TOS GPIO: set %x to %x \n", (int)offset+0x18, *(s->gpio_shm+(int)offset/4+6)) ;
+    } else if (((int)offset == 0x28) || ((int)offset == 0x2c)) {
+        // write to 0x34 or 0x38
+        *(s->gpio_shm+(int)offset/4+3) &= ~(unsigned)value;
+        fprintf(stderr,"[QEMU] TOS GPIO: clear %x to %x \n", (int)offset+0x0c, *(s->gpio_shm+(int)offset/4+6)) ;
+    } 
 }
 
 static const MemoryRegionOps bcm2835_todo_ops = {
@@ -77,13 +84,19 @@ static int bcm2835_todo_init(SysBusDevice *sbd)
     key_t key = 6666;
 
     if ((shmid = shmget(key, GPIO_MEM_SIZE, IPC_CREAT | 0666)) < 0 ) {
-        printf("[QEMU] TOS GPIO failed to create share memory)\n");
+        fprintf(stderr, "[QEMU] TOS GPIO failed to create share memory)\n");
     }
 
     s->gpio_shm = shmat(shmid, NULL, 0);
     if (s->gpio_shm == (int *) -1) {
-        printf("[QEMU] TOS Attach share memory failed\n");
+        fprintf(stderr, "[QEMU] TOS Attach share memory failed\n");
     }
+    
+    int i;
+    /* Reset shared memory */
+    for (i = 0; i < 40; i ++) {
+        *(s->gpio_shm+i) = 0; 
+    }   
 
     return 0;
 }

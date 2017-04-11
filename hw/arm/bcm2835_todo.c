75f0d1c1410396e16859cac5210cd4fed2c8d5c4
YEQING YAN <yyan1@mail.sfsu.edu>
2017-04-10 19:47:35 -0700

    update gpio in qemu

--- hw/arm/bcm2835_todo.c
+++ hw/arm/bcm2835_todo.c
@@ -6,11 +6,13 @@
 #include "hw/sysbus.h"
 #include <sys/types.h>         
 #include <sys/socket.h>
+#include "qapi/qmp/qjson.h"
 #include <string.h>
 #include <netdb.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
+
 #define TYPE_BCM2835_TODO "bcm2835_todo"
 #define BCM2835_TODO(obj) \
         OBJECT_CHECK(bcm2835_todo_state, (obj), TYPE_BCM2835_TODO)
@@ -23,16 +25,24 @@
 #define TOS_READ_PIN 18
 #define TOS_WRITE_PIN 15
 
+// GPIO Registers offset
+#define GPFSEL0 0x0
+#define GPFSEL1 0x4
+#define GPFSEL2 0x8
+#define GPFSEL3 0xC
+#define GPFSEL4 0x10
+#define GPFSEL5 0x14
+#define GPSET0  0x1C
+#define GPSET1  0x2D
+#define GPCLR0  0x28
+#define GPCLR1  0x2C
+#define GPLEV0  0x34
+#define GPLEV1  0x38
+
 typedef struct {
     SysBusDevice busdev;
     MemoryRegion iomem;
-    int gpio7_terminal_read;
-    int gpio15_tos_write;
-    int gpio14_tos_bit;
-    int bit_index;
-    int str_index;
-    unsigned int bits[8];
-    char str[256];
+    int socketfd;
 } bcm2835_todo_state;
 
 /* Get pin number */
@@ -51,10 +61,11 @@ static int get_pin(uint64_t value) {
 }
 
 static int setup_socket(void) {
-    const int SERVER_PORT = 8989;
+    const int SERVER_PORT = 8988;
     struct sockaddr_in serv_addr;
     struct hostent *local;
     int socketfd;
+    fprintf(stderr, "start setup socket\n");
 
     local = gethostbyname("localhost");
     if (local == NULL) {
@@ -75,131 +86,66 @@ static int setup_socket(void) {
     serv_addr.sin_port = htons(SERVER_PORT);
 
     if (connect(socketfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
-        fprintf(stderr, "[QEMU][Raspi] ERROR connecting(%d): %s", errno, strerror(errno));
+        fprintf(stderr, "[QEMU][Raspi] ERROR connecting(%d): %s \n", errno, strerror(errno));
         exit(1);
     }
     return socketfd;
 }
 
-/* Send string through socet */
-static void send_string(char *str, int len) {
-    int n;
-    int socket_fd;
-    char buf[256];
-    
-    socket_fd = setup_socket();
-    
-    n = write(socket_fd, str, len);
-
-    if (n < 0) {
-        fprintf(stderr, "[qemu][raspi] error writing to socket\n");
-        exit(1);
-    }    
-
-    /* Read response */
-    memset(buf, 0, sizeof(buf));
-    n = read(socket_fd, buf, 255);
-
-    if (n < 0) {
-        fprintf(stderr, "[QEMU][Raspi] TODO WRITE ERROR %s reading from socket %d\n", strerror(errno), n);
-        exit(1);
-    }    
-
-    if (strcmp(buf, "OK") != 0){
-        fprintf(stderr, "[QEMU][Raspi] ERROR response from socket\n");
-        exit(1);
-    }
-
-    fprintf(stderr,"[QEMU] TOS GPIO: write %s to GPIO server\n", str);
-    close(socket_fd);
-}
-
-
 static uint64_t read_gpio(bcm2835_todo_state *s, unsigned int offset) {
+    char buf[256];
     unsigned int value = 0;
-    if (offset != 0x34) {
+    int n;
+
+    if ((offset != GPLEV0) && (offset != GPLEV1)) {
         fprintf(stderr, "[QEMU][TOS] Warnning! GPIO read from unknown offset %x!", offset);
         return (uint64_t)0xffffffff;
     }
 
-    value = value | ((s->gpio7_terminal_read) << TERM_READ_PIN);
-    value = value | ((s->gpio14_tos_bit) << TOS_BIT_PIN);
-    value = value | ((s->gpio15_tos_write)  << TOS_WRITE_PIN);
+    /* Send msg to server */
+    bzero(buf, 256);
+    sprintf(buf, "#R\n");
+
+    n = write(s->socketfd, buf, strlen(buf));
+
+    if (n < 0) {
+        fprintf(stderr, "[QEMU][Raspi] ERROR %s writing to socket\n", strerror(errno));
+        exit(1);
+    }    
+
+    // /* Read response */
+    // bzero(buf, 256);
+    // n = read(s->socketfd, buf, 255);
+
+    // if (n < 0) {
+    //     fprintf(stderr, "[QEMU][Raspi] READ ERROR %s reading from socket %s\n", strerror(errno), buf);
+    //     exit(1);
+    // }    
+
+    // value = atoi(buf);
+    // fprintf(stderr, "[QEMU] TOS GPIO: read %d from %x\n", value, (unsigned int)offset);
+
+    fprintf(stderr, "[QEMU][TOS] Warnning! FIXME, add logic for read");
     return (uint64_t) value; 
 }
 
-static void write_char(bcm2835_todo_state *s) {
-    char v = 0;
-    int i = 0, j = 7;
-    
-    if (s->str_index > 255) {
-        fprintf(stderr, "[QEMU][TOS] Reach string buffer limit!"); 
-        return;
-    }
-
-    for (i=0,j=7; i<8; i++,j--) {
-        v += (s->bits[i] << j);
-    }
-    
-    s->str[s->str_index] = v;
-    s->str_index += 1;
-    
-    if(v == 0) {
-        // If meet '\0', send string and reset string buffer
-        send_string(s->str, s->str_index);
-        memset(s->str, 0, sizeof(s->str));
-        s->str_index = 0;
-    }
-    return;
-}
-
 static void write_gpio(bcm2835_todo_state *s, uint64_t value, short bit) {
-    int pin; 
+    char buf[256];
+    int n, pin; 
     
     pin = get_pin(value);
-    switch (pin) {
-        
-            case TOS_BIT_PIN:
-            if ((s->gpio7_terminal_read == -1) || (s->gpio15_tos_write == -1)) {
-                // Initialize haven't done yet.
-                return;
-            }
+        /* Send msg to server */
+    bzero(buf, 256);
+    sprintf(buf, "PIN%dW%d\n", pin, bit);
 
-            if (s->bit_index >= 8) {
-                fprintf(stderr, "[QEMU][TOS] Warning! Bit buffer full!\n");
-                return; 
-            }
-            s->bits[s->bit_index] = bit;
-            s->bit_index += 1;
-            break;
-        
-        case TERM_READ_PIN:
-            /* TERM_READ_PIN should be read only */
-            break;
+    n = write(s->socketfd, buf, strlen(buf));
 
-        case TOS_WRITE_PIN:
-            if (s->gpio15_tos_write == -1) {
-                s->gpio15_tos_write = bit;
-                fprintf(stderr, "[QEMU][TOS] Initialize: PIN 15 TOS_WRITE = %d\n", (int)bit);
-                return; 
-            }
-            s->gpio15_tos_write = bit;
-            if(bit == 1) {
-                if (s->bit_index >= 8) {
-                    write_char(s);
-                    memset(s->bits, 0, sizeof(s->bits));
-                    s->bit_index = 0;
-                }
-                s->gpio7_terminal_read = 1;
-            } else {
-                s->gpio7_terminal_read = 0;
-            }
-            break;
-        case 18:
-            break;
-        default:
-            fprintf(stderr, "[QEMU][Raspi] Warnning! write to unknown pin %d\n", pin);
-    }     
+    if (n < 0) {
+        fprintf(stderr, "[QEMU][Raspi] ERROR %s writing to socket\n", strerror(errno));
+        exit(1);
+    }    
+
+    return;
 }
 
 
@@ -209,60 +155,28 @@ static void write_gpio(bcm2835_todo_state *s, uint64_t value, short bit) {
 static uint64_t bcm2835_todo_read(void *opaque, hwaddr offset,
     unsigned size)
 {
-    int n;
-    unsigned int value;
-    const char *cmd = NULL;
-    char buf[256];
+    uint64_t value;
     bcm2835_todo_state *s = (bcm2835_todo_state *)opaque;
- 
-    int socketfd = 0;
 
     switch (offset) {
-        case 0x0:
-        case 0x4:
-        case 0x8:
-        case 0xC:
-        case 0x10:
-        case 0x14:
+        case 0x0: //FS0
+        case 0x4: //FS1
+        case 0x8: //FS2
+        case 0xC: //FS3
+        case 0x10: //FS4
+        case 0x14: //FS5
             return 0;
-
-        case 0x34:
-        case 0x38:
-            return read_gpio(s, (unsigned int)offset);
-        case 0x94:
-            return 0;
-
+	    case GPLEV0: 
+	        value = read_gpio(s, (unsigned int)offset);  //GPLEV0
+        case GPLEV1:
+            value = read_gpio(s, (unsigned int)offset);  //GPLEV1
+        case 0x94:  //GPPUD 
+            value = 0;
         default:
-            cmd = "";
             fprintf(stderr, "[QEMU][Raspi] Warning Read from unknown offset %x!\n", (unsigned int)offset);
-            return 0xffffffff;
+            value = 0xffffffff;
     }
-    
-    /* Send msg to server */
-    bzero(buf, 256);
-    sprintf(buf, "%s#R", cmd);
-
-    socketfd = setup_socket();
-    n = write(socketfd, buf, strlen(buf));
-
-    if (n < 0) {
-        fprintf(stderr, "[QEMU][Raspi] ERROR %s writing to socket\n", strerror(errno));
-        exit(1);
-    }    
-
-    /* Read response */
-    bzero(buf, 256);
-    n = read(socketfd, buf, 255);
-
-    if (n < 0) {
-        fprintf(stderr, "[QEMU][Raspi] READ ERROR %s reading from socket %s\n", strerror(errno), buf);
-        exit(1);
-    }    
-
-    value = atoi(buf);
-    close(socketfd);
-    fprintf(stderr, "[QEMU] TOS GPIO: read %d from %x\n", value, (unsigned int)offset);
-    return (uint64_t)value;
+    return value;
 }
 
 /*
@@ -277,25 +191,26 @@ static void bcm2835_todo_write(void *opaque, hwaddr offset,
     bcm2835_todo_state *s = (bcm2835_todo_state *)opaque;
     
     switch (offset) {
-        case 0x0:
-        case 0x4:
-        case 0x8:
-        case 0xC:
-        case 0x10:
-        case 0x14:
-            return ;
+        case GPFSEL0: // fprintf(stderr, "Function Select 0\n"); break;  //FS0  pin 0-9
+        case GPFSEL1: //fprintf(stderr, "Function Select 1\n");  break;  //FS1 pin 10-19
+        case GPFSEL2: //FS2  pin 20-29
+        case GPFSEL3: //FS3  pin 30-39
+        case GPFSEL4: //FS4  pin 40-49
+        case GPFSEL5: //FS5  pin 50-53
+            break ;
 
-        case 0x1C:
-            write_gpio(s, value, 1);
+        case GPSET0:
+            write_gpio(s, value, 1);  // Pin Output Set 0
             break;
-        case 0x20:        
-            fprintf(stderr, "[QEMU][Raspi] GPIO write 1 to PIN set 1!\n");
-            return;
-        case 0x28:
-            write_gpio(s, value, 0);
+        case GPSET1:        
+            fprintf(stderr, "[QEMU][Raspi] Warning! Write to Pin 32-53 not set!\n");
             break;
-        case 0x2C:
-            fprintf(stderr, "[QEMU][Raspi] GPIO write 0 to PIN set 1!\n");
+        case GPCLR0: 
+	        // fprintf(stderr, "Pin Output Clear 0\n"); break;
+            write_gpio(s, value, 0);  // Pin Output Clear 0
+            break;
+        case GPCLR1:
+            fprintf(stderr, "[QEMU][Raspi] Warning! Write to Pin 32-53 not set!\n");
             break;
         case 0x94:
             break;
@@ -342,15 +257,7 @@ static int bcm2835_todo_init(SysBusDevice *sbd)
 
     vmstate_register(dev, -1, &vmstate_bcm2835_todo, s);
 
-    /* initialize gpio pins */
-    s->gpio7_terminal_read = 0;
-    s->gpio14_tos_bit = 0;
-    s->gpio15_tos_write = -1;
-    s->bit_index = 0;
-    s->str_index = 0;
-    memset(s->bits, 0, sizeof(s->bits));
-    memset(s->str, 0, sizeof(s->str));
-    
+    s->socketfd = setup_socket();
     return 0;
 
 }
